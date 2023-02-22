e7612ba8b3288a0af991575a5db58f34efa5b62e
Zhijia <zhijiaxzhang@gmail.com>
2023-02-22 00:38:57 -0500

    finished branding snippet function

--- app/app.py
+++ app/app.py
@@ -1,16 +1,55 @@
 import os
 import openai
 from dotenv import load_dotenv
+import argparse
 
 load_dotenv() #load files from .env file
-
-thing = 'coffee'
-prompt = f'Generate a catchy branding snippet for {thing}'
-prompt2 = f'Generate a list audiences for my brand {thing}'
 openai.api_key = os.getenv("OPENAI_APIKEY")
 
-response = openai.Completion.create(engine="text-davinci-003", prompt=prompt, max_tokens=24)
 
-response2 = openai.Completion.create(engine="text-davinci-003", prompt=prompt2, max_tokens=32)
 
-print(response2)
+def main():
+    print("app is running!")
+
+    #unlike java's public static void main str[] args, python doesn't pick up 
+    #commands from the terminal so we have to parse them ourselves using argparse module
+
+    #create parser object
+    parser =  argparse.ArgumentParser(description="parses some input as a string")
+
+    #gives direction to the parser 
+    #capture everything that comes after i
+    #converts it to a string
+    #required to have a input
+    parser.add_argument('-i', type=str, required=True)
+
+    #parse the args
+    args = parser.parse_args()
+    input = args.i
+
+    res = generateContent(input)
+    print(res)
+
+
+
+def generateContent(input : str) -> str:
+
+    prompt = f'generate a upbeat and catchy branding snippet for {input}'
+    response = openai.Completion.create(
+        engine="text-davinci-003", prompt=prompt, max_tokens=24)
+    return(getContent(response))
+
+
+def getContent(response : dict) -> str:
+
+    #get content and strip whitespace
+    content = response.get('choices')[0].get('text')
+    return(content.strip().strip("\""))
+
+
+
+if __name__ == "__main__":
+    main()
+
+
+
