d55c99189fc8bb3a7664c9531b9562d82e7ca920
Zhijia <zhijiaxzhang@gmail.com>
2023-02-23 00:16:39 -0500

    finished app.py

--- app/app.py
+++ app/app.py
@@ -3,48 +3,73 @@ import openai
 from dotenv import load_dotenv
 import argparse
 
+
 load_dotenv() #load files from .env file
 openai.api_key = os.getenv("OPENAI_APIKEY")
 
 
 
 def main():
-    print("app is running!")
-
-    #unlike java's public static void main str[] args, python doesn't pick up 
-    #commands from the terminal so we have to parse them ourselves using argparse module
-
-    #create parser object
     parser =  argparse.ArgumentParser(description="parses some input as a string")
-
-    #gives direction to the parser 
-    #capture everything that comes after i
-    #converts it to a string
-    #required to have a input
     parser.add_argument('-i', type=str, required=True)
-
-    #parse the args
     args = parser.parse_args()
     input = args.i
 
-    res = generateContent(input)
-    print(res)
+    if validateInput(input):
+        generateContent(input)
+        generateKeywords(input)
+    else:
+        print("your input is too long! must be under 30 characters")
 
 
 
 def generateContent(input : str) -> str:
-
     prompt = f'generate a upbeat and catchy branding snippet for {input}'
     response = openai.Completion.create(
-        engine="text-davinci-003", prompt=prompt, max_tokens=24)
-    return(getContent(response))
+        engine="text-davinci-003", prompt=prompt, max_tokens=32)
+    print(getContent(response))
+
+
+def generateKeywords(input: str):
+    prompt = f'generate related branding keywords for {input} in a string format and in lowercase'
+    response = openai.Completion.create(
+    engine="text-davinci-003", prompt=prompt, max_tokens=40)
+    print(parseKeywords(response))
+
 
 
 def getContent(response : dict) -> str:
-
     #get content and strip whitespace
     content = response.get('choices')[0].get('text')
-    return(content.strip().strip("\""))
+    content = content.strip().strip("\"")
+    punc = {"!", ".", "?"}
+    i = len(content) - 1
+    while i> 0:
+        if content[i] in punc:
+            break
+        i -= 1
+    
+    #sentence might be cut off due to token limit, so return the full sentences
+    return content[:i + 1]
+        
+
+
+def parseKeywords(response: dict):
+    content = response.get('choices')[0].get('text')
+    content = content.strip().strip("\"")
+    keywords = content.split(",")
+    for x in range(len(keywords)):
+        keywords[x] = keywords[x].strip()
+
+    #last word might be cut off due to token limit so omit last word for consistency
+    return keywords[:-1]
+
+
+#person using the app could intentionally enter something bad to burn our openai credits
+def validateInput(input: str) -> bool:
+    return len(input) <= 30
+        
+
 
 
 
